f93a6effe57deb9351583d47ade1d141e315836d
Thien Tran <thientran98qb@gmail.com>
2020-11-04 13:56:14 +0700

    Devide Layouts

--- views/users/index.php
+++ views/users/index.php
@@ -1,33 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <table>
-        <thead>
-            <tr>
-                <td>Stt</td>
-                <td>User Name</td>
-                <td>Thumb</td>
-                <td>Phone</td>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($userList as $key => $value) {  ?>
-            <tr>
-                <td><?php echo $value['id']; ?></td>
-                <td><?php echo $value['user_name']; ?></td>
-                <td><?php echo $value['user_img']; ?></td>
-                <td><?php echo $value['user_phone']; ?></td>
-            </tr>
-            <?php }?>
-        </tbody>
-            <tr></tr>
-        <tfoot>
-        </tfoot>
-    </table>
-</body>
-</html>
+<?php include_once DOCUMENT_ROOT. "/views/layouts/header.php"; ?>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <td>Stt</td>
+                    <td>User Name</td>
+                    <td>Thumb</td>
+                    <td>Phone</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($userList as $key => $value) {  ?>
+                <tr>
+                    <td><?php echo $value['id']; ?></td>
+                    <td><?php echo $value['user_name']; ?></td>
+                    <td><?php echo $value['user_img']; ?></td>
+                    <td><?php echo $value['user_phone']; ?></td>
+                </tr>
+                <?php }?>
+            </tbody>
+            <tfoot>
+            </tfoot>
+        </table>
+    </div>
+<?php include_once DOCUMENT_ROOT. "/views/layouts/footer.php"; ?>
