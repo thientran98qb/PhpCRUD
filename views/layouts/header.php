2d33c0b32df753d1eb762ad81607a80a8245d626
Thien Tran <thientran98qb@gmail.com>
2020-11-04 14:11:39 +0700

    add css file

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -5,8 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Document</title>
 </head>
 <body>
     <header>
+        <div class="nav">
+            <div class="nav_content">
+                <h1>CRUD PHP</h1>
+            </div>
+        </div>
     </header>
