ab000c649814f6a10830978f7939234fd418fabf
Thien Tran <thientran98qb@gmail.com>
2020-11-08 22:31:02 +0700

    show ajax

--- model/ProductModel.php
+++ model/ProductModel.php
@@ -7,7 +7,16 @@
         }
         //show Product detail
         function show(){
-            $query="SELECT * FROM {$this->table} INNER JOIN users ON products.user_id=users.user_id";
+            $query="SELECT * FROM {$this->table} INNER JOIN users ON products.user_id=users.user_id ORDER BY {$this->table}.product_id DESC";
+                $stmt=$this->conn->prepare($query);
+                $stmt->execute();
+                if($stmt->rowCount()>0){
+                    $data=$stmt->fetchAll(PDO::FETCH_ASSOC);
+                }
+                return $data;
+            }
+        function showData($start=0,$limit=4){
+        $query="SELECT * FROM {$this->table} INNER JOIN users ON products.user_id=users.user_id ORDER BY {$this->table}.product_id DESC LIMIT {$start},{$limit}";
             $stmt=$this->conn->prepare($query);
             $stmt->execute();
             if($stmt->rowCount()>0){
@@ -17,5 +26,12 @@
         }
         function addProduct($data){
             return $this->add($data,$this->table);
+        }
+        function totalProduct(){
+            $query="SELECT * FROM {$this->table}";
+            $stmt=$this->conn->prepare($query);
+            $stmt->execute();
+            $count=$stmt->rowCount();
+            return $count;
         }
     }
